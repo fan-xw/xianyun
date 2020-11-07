8ce1c01796975350db873eccf3f571dcdfeea581
fanxiaowei <18336200155@163.com>
2020-11-07 16:20:32 +0800

    13-搜索框功能的全部实现

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,36 @@
             </el-carousel-item>
         </el-carousel>
         <!-- 下面是其他内容，装备写搜索框 -->
+        <div class="banner-content">
+            <div class="search-bar">
+                
+                <!-- tab栏 -->
+                <el-row 
+                type="flex" 
+                class="search-tab">
+                    <span 
+                    v-for="(item, index) in options" 
+                    :key="index" 
+                    :class="{active: index === currentOption}"
+                    @click="handleOption(index)">
+                        <i>{{item.name}}</i>
+                    </span>
+                </el-row>
+    
+                
+                <!-- 输入框 -->
+                <el-row 
+                type="flex" 
+                align="middle" 
+                class="search-input">
+                    <input 
+                    :placeholder="options[currentOption].placeholder" 
+                    v-model="searchValue"
+                    @keyup.enter="handleSearch"/>
+                    <i class="el-icon-search" @click="handleSearch"></i>
+                </el-row>
+            </div>
+        </div>
    </div>
 </template>
 
@@ -25,7 +55,25 @@ export default {
     data(){
         return {
             // 轮播图数据
-            banners: []
+            banners: [],
+            options: [      // 搜索框tab选项
+                {
+                    name: "攻略", 
+                 	placeholder: "搜索城市", 
+                 	pageUrl: "/post?city="
+                },
+                {
+                    name: "酒店", 
+                    placeholder: "请输入城市搜索酒店", 
+                    pageUrl: "/hotel?city="},
+                {
+                    name: "机票", 
+                    placeholder: "请输入出发地", 
+                    pageUrl: "/air"
+                }
+            ],
+            searchValue: "",    // 搜索框的值
+            currentOption: 0,   // 当前选中的选项    
         }
     },
 
@@ -58,6 +106,30 @@ export default {
          img.url = res.config.baseURL + img.url
          return img
       });
+   },
+
+   methods:{
+
+       // 1.切换tab栏时触发    
+       handleOption(index){
+           // 设置当前tab
+           this.currentOption = index;
+           const item = this.options[index]
+           if (item.name === '机票') {
+               // 路由传值 
+               return this.$router.push(item.pageUrl)
+           }   
+       },
+
+       // 2.点击搜索按钮进行跳转页面    
+       handleSearch () {
+        // 拿着当前用户输入的数据，跳转页面，进行搜索   
+        console.log(this.searchValue,'是当前搜索关键字');
+        // 进行页面的跳转
+        // 是酒店就跳去 /hotel,是攻略就跳去 /post
+        // 当前被激活的分类：this.options[this.currentOption]
+        this.$router.push(this.options[this.currentOption].pageUrl + this.searchValue)
+       },
    }
 }
 </script>
@@ -75,6 +147,95 @@ export default {
     .banner-image{
         width:100%;
         height:100%;
+    }
+
+    .banner-content{
+        z-index:9;
+        width:1000px;
+        position:absolute;
+        left:50%;
+        top:45%;
+        margin-left: -500px;
+        border-top:1px transparent solid;
+
+        .search-bar{
+            width:552px;
+            margin:0 auto;
+        }
+
+        .search-tab{
+            .active{
+                i{
+                color:#333;
+                }
+                &::after{
+                background: #eee;
+                }
+            }
+
+            span{
+                width:82px;
+                height:36px;
+                display:block;
+                position: relative;
+                margin-right:8px;
+                cursor: pointer;
+
+                i{
+                position:absolute;
+                z-index:2;
+                display: block;
+                width:100%;
+                height:100%;
+                line-height:30px;
+                text-align:center;
+                color:#fff;
+                }
+
+                &:after{
+                position: absolute;
+                left:0;
+                top:0;
+                display:block;
+                content: "";
+                width:100%;
+                height:100%;
+                border: 1px rgba(255,255,255,.2) solid;
+                border-bottom: none;
+                transform: scale(1.1,1.3) perspective(.7em) rotateX(2.2deg);
+                transform-origin: bottom left;
+                background: rgba(0,0,0,.5);
+                border-radius:1px 2px 0 0;
+                box-sizing:border-box;
+                }
+            }
+        }
+
+        .search-input{
+            width:550px;
+            height:46px;
+            background:#fff;
+            border-radius: 0 4px 4px 4px;
+            border: 1px rgba(255,255,255,.2) solid;
+            border-top:none;
+            box-sizing: unset;
+
+            input{
+                flex:1;
+                height:20px;
+                padding: 13px 15px;
+                outline: none;
+                border:0;
+                font-size:16px;
+            }
+
+            .el-icon-search{
+                cursor :pointer;
+                font-size:22px;
+                padding:0 10px;
+                font-weight:bold;
+            }
+        }
     }
 }
 </style>
