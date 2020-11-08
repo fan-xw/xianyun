452388f79fe56e355603563633bff8a89fa957bd
fanxiaowei <18336200155@163.com>
2020-11-09 00:37:36 +0800

    36-nuxt框架里面的axios.onError拦截器处理错误的方法，以及测试错误页面test

---
+++ pages/test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="login">登录</button>
+  </div>
+</template>
+
+<script>
+import { login } from '@/apis/user.js'
+export default {
+  data () {
+    return {
+      user: {
+              username: '13444444444',
+              password: '2948041'
+            }  
+    }
+  },
+  methods:{
+    login () {
+      this.$axios({
+                method: 'post',
+                url: '/accounts/login',
+                data: {
+                    username: '13444444444',
+                    password: '2948041'
+                }
+      }).then(res=>{
+          console.log(res.data);
+          // 添加 .catch(() => {}) 把错误给捕获掉
+      }).catch(()=>{})
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -5,13 +5,24 @@
 // onResponseError(err) 在响应中(错误)
 // 默认情况下，这些函数不必返回任何内容
 
+// 单独引入 Message
+import { Message } from 'element-ui';
 
 // 如果在插件里面需要获取到 nuxt 本身
 // 需要 export default,  如果我们知道要什么，比如axios，就可以直接解构出来
 
-export default function({$axios}) {
+export default function({$axios, redirect}) {
+    // $axios.onError 是一个 nuxt 提供的辅助拦截器函数
+    // 里面可以拦截到错误
     $axios.onError(err=>{
         // 这里是每当出错的时候都会被拦截
-        console.log(err);
+        console.dir(err);
+        console.log('错误信息是:' + err.response.data.message);
+        // 弹出一个提示给用户
+        // this.$message.error('')在这里由于不是组件内部，没有 this可以用，所以我们只能采用单独引入 Element-ui当中的 Message 组件
+        Message.error(err.response.data.message || '系统错误')
+
+        // 如果需要跳转页面，比如说跳转到登录页: 可以从 nuxt 中拿到 redirect函数即可
+        redirect('/user/login')
     })
 }
