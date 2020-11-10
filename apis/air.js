eb3aacaa5ebe7af5093566733e334d0df2435e59
fanxiaowei <18336200155@163.com>
2020-11-10 10:16:21 +0800

    46-封装机票城市请求的api

--- apis/air.js
+++ apis/air.js
@@ -1,11 +1,11 @@
 import axios from '@/utils/http'
 
 // 实时机票城市api请求
-export const airCity = (params) => {
+export const airCity = (name) => {
     return axios({
         url:'/airs/city',
         params:{
-            name:''
+            name
         }
     })
 }
