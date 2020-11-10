063d063ef07695c7105295094a4009bdf608b53b
fanxiaowei <18336200155@163.com>
2020-11-10 23:42:33 +0800

    62-完成机票列表页的基本布局、航班头部组件准备(引入、注册、渲染)

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,14 +1,54 @@
 <template>
-  <div>机票搜索结果页</div>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    过滤条件
+                </div>
+                
+                <!-- 航班头部布局 -->
+                <FlightsListHead />
+                
+                
+                <!-- 航班信息 -->
+                <div>
+                    航班信息
+                </div>
+
+                <!-- 分页组件 -->
+                <div>
+                  分页组件
+                </div>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+                侧边栏
+            </div>
+
+
+        </el-row>
+    </section>
 </template>
 
 <script>
 import { airsList } from '@/apis/air.js'
+// 👌1.引入飞机列表页头部组件
+import FlightsListHead from '@/components/air/FlightsListHead'
 export default {
   data() {
     return {
       flightsDate:{}
     }
+  },
+
+  components:{
+    // 👌2.注册飞机列表页头部组件
+     FlightsListHead
   },
 
   async created() {
@@ -20,6 +60,18 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
 
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
 </style>
