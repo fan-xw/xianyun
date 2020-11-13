28df4fe90d7ca17c992f968edf9b31603b60117a
fanxiaowei <18336200155@163.com>
2020-11-13 17:52:57 +0800

    101-新建表单组件并完成基本的布局和样式，在机票订单页 引入 注册 渲染

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -2,9 +2,7 @@
       <div class="container">
         <el-row type="flex" justify="space-between">
             <!-- 订单表单 -->
-            <div class="main">
-                订单表单
-            </div>
+            <OrderForm/>
 
             <!-- 侧边栏 -->
             <div class="aside">
@@ -15,8 +13,11 @@
 </template>
 
 <script>
+import OrderForm from '@/components/air/OrderForm'
 export default {
-
+    components: {
+        OrderForm
+    }
 }
 </script>
 
