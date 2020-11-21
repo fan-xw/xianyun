f30ced4fc8a94aeea8e6a85058d442c620aa4970
fanxiaowei <18336200155@163.com>
2020-11-21 09:20:45 +0800

    利用 父传子 把数据传递过去

--- components/hotel/HotelLocation.vue
+++ components/hotel/HotelLocation.vue
@@ -131,6 +131,14 @@ export default {
             return [];
           },
         },
+
+        // 酒店详情数据
+        hotelList: {
+          type: Array,
+          default: () => {
+            return [];
+          },
+        },
     },
 
     data() {
@@ -189,7 +197,85 @@ export default {
               });
             }
         }
+    },
+
+    watch: {
+        // 监听酒店详情数据的变化，去更改地图定位信息
+        hotelList() {
+            // 调用地图
+            this.init();
+            console.log(this.hotelList);
+            if (this.hotelList.length != 0) {
+              this.map.setCenter([
+                this.hotelList[0].location.longitude,
+                this.hotelList[0].location.latitude,
+              ]);
+
+            // 清除地图覆盖物
+            this.map.clearMap();
+            let markerList = [];
+            
+            // 添加点标记
+            this.hotelList.forEach((v, i) => {
+                // 创建一个 Marker 实例：
+                var marker = new AMap.Marker({
+                    content: `<span class="marker">${i + 1}</span>`,
+                    // 位置坐标
+                    // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+                    position: new AMap.LngLat(
+                      v.location.longitude,
+                      v.location.latitude
+                    ), 
+                    // 提示    
+                    title: v.name,
+                });
+
+          // 移进点标记打开信息窗口
+    //       marker.on("mouseover", () => {
+    //         // 创建 infoWindow 实例
+    //         let infoWindow = new AMap.InfoWindow({
+    //           position: new AMap.LngLat(
+    //             v.location.longitude,
+    //             v.location.latitude
+    //           ), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+    //           content: `<span style="font-size: 12px;color: #333">${v.name}</span>`, //传入 dom 对象，或者 html 字符串
+    //           offset: new AMap.Pixel(0, -35),
+    //         });
+
+    //         // 打开信息窗体
+    //         infoWindow.open(this.map);
+    //       });
+    //       // 移出点标记关闭信息窗口
+    //       marker.on("mouseout", () => {
+    //         this.map.clearInfoWindow();
+    //       });
+    //       markerList.push(marker);
+    //     });
+    //     this.map.add(markerList);
+    //   } else {
+    //     // console.log(this.$route.query.cityName);
+    //     // 正向获取地理编码
+    //     AMap.plugin("AMap.Geocoder", () => {
+    //       var geocoder = new AMap.Geocoder({
+    //         // city 指定进行编码查询的城市，支持传入城市名、adcode 和 citycode
+    //         city: this.$route.query.cityName,
+    //       });
+
+    //       geocoder.getLocation(this.$route.query.cityName, (status, result) => {
+    //         if (status === "complete" && result.info === "OK") {
+    //           // result中对应详细地理坐标信息
+    //           console.log(result);
+    //           this.map.setCenter([
+    //             result.geocodes[0].location.lng,
+    //             result.geocodes[0].location.lat,
+    //           ]);
+    //         }
+    //       });
+        });
+      }
+        },
     }
+
 }
 </script>
 
